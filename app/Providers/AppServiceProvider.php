dfaa36c72e0b30d34e2481991dcebe81f9a3d4d6
Gustavo Silva Santana <gustavo_ssantana@hotmail.com>
2020-08-20 14:29:08 -0300

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        URL::forceScheme('https');
+        $this->app['request']->server->set('HTTPS','on');
 
     }
 }
