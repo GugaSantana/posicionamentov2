fe37db880646c78e38f0efc91ec7d941412af05a
Gustavo Silva Santana <gustavo_ssantana@hotmail.com>
2020-10-26 19:34:30 -0300

    Revisão texto carta de apresentação

--- resources/views/manual_intro.blade.php
+++ resources/views/manual_intro.blade.php
@@ -45,21 +45,23 @@
 <img src="img/logo_capa.png" style="float: left;width: 30%;margin-top: 2%;">
 <img src="../posicionamento/public/img/selo_top_seller.png" style="float: right;width: 12%;">
 
-<h2 style="color: #35408f;text-align: center;margin-top: 10%">Bem vindo ao Programa TOP SELLER<h2>
+<h2 style="color: #35408f;text-align: center;margin-top: 10%">Bem-vindo ao Programa TOP SELLER!<h2>
         
-<h3 style="color: #35408f; text-align: center">Parabéns!!!, você adquiriu uma metodologia que mudará sua vida profissional!</h3>
+<h3 style="color: #35408f; text-align: center">Parabéns! Você adquiriu uma Metodologia que mudará sua vida profissional!</h3>
 <br>
-<p>Pela primeira vez é possível desenvolver todas as etapas das Vendas Consultivas apoiado em uma série única de instrumentos desenvolvidas pela Posicionamento, uma empresa de consultoria em competitividade que opera no mercado brasileiro e internacional desde 1986.</p>
-<p>Transferimos e adaptamos para o meio digital nossas mais avançadas técnicas para aumentar a Produtividade das Vendas já transferidas presencialmente a mais de 1.000 profissionais, para que você possa adquirir de maneira sistemática e na velocidade que você determinar, todas as competências necessárias para implementar as Vendas Consultivas.</p>
-<p>Um novo paradigma de vendas tem surgido para desenvolver a atividade de vendas, no mundo da Quarta Revolução Industrial, no qual já estamos inseridos.</p>
-<p>Mudanças de paradigmas se impõem em todas as profissões e em vendas, esta mudança de paradigma consiste em passar das Vendas Transacionais para as Vendas Consultivas.</p>
-<p>E esta é sua oportunidade única de migrar sua prática profissional da VENDA TRADICIONAL para a VENDA CONSULTIVA porque você efetivamente vai adquirir as competências para praticar Vendas Consultivas através de 20 instrumentos específicos, objetivos, quantitativos e que lhe permitirão desenvolver Planos de Ação e medir resultados.</p>
-<p>Este programa é metodologia pura!</p>
-<p>Aqui não se trata de vídeos de palestras ou estudo de casos, muitas vezes distantes de sua realidade: aqui você só recebe técnicas, método em bases quantitativas, o que lhe permitirá mensurar seu progresso.</p>
-<p>Nesta apresentação da metodologia você terá informações para entender o que são as Vendas Consultivas, e contextualizar os 20 instrumentos lhe que serão transferidos.</p>
-<p>Você saberá quais são as etapas das Vendas Consultivas e que instrumentos contribuem para melhorar seu desempenho em cada uma das etapas.</p>
-<p>Obrigado por aderir a esta metodologia.</p>
-<p>Desejamos a você o maior sucesso em adotar e implementar as técnicas propostas: <font style="color: #35408f">Sua prática profissional mudará de paradigma.</font></p>
+<p>Pela primeira vez, é possível desenvolver todas as etapas das Vendas Consultivas com esta nova Metodologia aqui apresentada.</p>
+<p>Este programa é apoiado em uma série única de Instrumentos, desenvolvidos pela Posicionamento, uma empresa de consultoria em competitividade que opera no mercado brasileiro e internacional desde 1986.</p>
+<p>Transferimos e adaptamos para o meio digital nossas mais avançadas técnicas, para aumentar a Produtividade das Vendas, já transferidas presencialmente a mais de 5.000 profissionais.</p>
+<p>Esta nova Metodologia entregará a você de maneira sistemática e na velocidade que você determinar, todas as competências necessárias para implementar as Vendas Consultivas.</p>
+<p>Um novo paradigma de vendas tem surgido para desenvolver a atividade de vendas, no mundo da Quarta Revolução Industrial, no qual estamos inseridos.</p>
+<p>Mudanças de paradigmas se impõem em todas as profissões e em vendas. Esta mudança de paradigma consiste em passar das Vendas Transacionais para as Vendas Consultivas.</p>
+<p>E esta é sua oportunidade única de migrar sua prática profissional da VENDA TRADICIONAL para a VENDA CONSULTIVA. No Programa TOP SELLER, você efetivamente vai adquirir as competências para praticar as Vendas Consultivas, através de 20 Instrumentos específicos, objetivos, quantitativos, que lhe permitirão desenvolver Planos de Ação e medir resultados.</p>
+<p>Este programa é Metodologia pura!</p>
+<p>O Programa TOP SELLER não se constitui de vídeos de palestras ou estudo de casos, muitas vezes distantes de sua realidade: aqui você só encontrará técnicas, um método em bases quantitativas, que lhe permitirá mensurar seu progresso.</p>
+<p>Nesta apresentação da Metodologia, você receberá informações para entender o que são as Vendas Consultivas e contextualizar os 20 Instrumentos que lhe serão transferidos.</p>
+<p>Você compreenderá quais são as etapas das Vendas Consultivas e que Instrumentos contribuirão para melhorar seu desempenho em cada uma dessas etapas.</p>
+<p>Obrigado por aderir a esta Metodologia!</p>
+<p>Desejamos a você o maior sucesso em adotar e implementar as técnicas propostas! <font style="color: #35408f">Sua prática profissional mudará de paradigma!</font></p>
 <br>
 
 @include('menu_intro')
