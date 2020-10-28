c75dd027752e1b1a074c3ccddef54d6a1d3c276b
Gustavo Silva Santana <gustavo_ssantana@hotmail.com>
2020-10-28 20:32:45 -0300

    Revisão texto instrumento 3

--- resources/views/instrumento3.blade.php
+++ resources/views/instrumento3.blade.php
@@ -31,12 +31,12 @@
                             <center>Levantamento de Informações</center>
                         </h4>
                         
-                        <p class="fonte18">No instrumento anterior, utilizando uma metodologia específica, definimos com precisão seus problemas prioritários.</p>
-                        <p class="fonte18">Assim, sabendo em que ordem você deve tratar de seus problemas, você otimizará seu tempo e aumentará sua produtividade.</p>
-                        <p class="fonte18">Neste instrumento vamos tratar de você: Você terá a oportunidade de fazer uma autoavaliação de seu perfil profissional e poderá concluir o potencial que você tem para melhorar seu desempenho, sua produtividade, enfim, sua eficácia como profissional de vendas. Nas recomendações serão dadas as instruções para converter este potencial em realidade.</p>
-                        <p class="fonte18">Todo profissional de Vendas apresenta diferentes níveis de excelência nas diversas competências requeridas para obter os melhores resultados.</p>
-                        <p class="fonte18">Este instrumento é de grande valor para visualizar seu perfil profissional e portanto, o nível em que se encontram suas competências para vendas.</p>
-                        <p class="fonte18">Os resultados serão muito específicos e objetivos</p>
+                        <p class="fonte18">No Instrumento anterior, utilizando uma Metodologia específica, definimos com precisão seus problemas prioritários.</p>
+                        <p class="fonte18">Assim, sabendo em que ordem você deve tratar de seus problemas, você otimizará seu tempo e aumentará sua Produtividade.</p>
+                        <p class="fonte18">Neste Instrumento vamos tratar de você: Aqui você terá a oportunidade de fazer uma autoavaliação de seu perfil profissional e poderá concluir o potencial que você tem para melhorar seu desempenho, sua produtividade, enfim, sua eficácia como Profissional de Vendas. Nas Recomendações serão dadas as instruções para converter este potencial em realidade.</p>
+                        <p class="fonte18">Todo Profissional de Vendas apresenta diferentes níveis de excelência nas diversas competências requeridas para obter os melhores resultados.</p>
+                        <p class="fonte18">Este Instrumento é de grande valor para visualizar seu perfil profissional e, portanto, o nível em que se encontram suas competências para Vendas.</p>
+                        <p class="fonte18">Os resultados serão muito específicos e objetivos.</p>
                             
 
                         </div>
@@ -44,32 +44,32 @@
                         <div class="col-12">
                             <p class="fonte18"><b>INSTRUÇÕES</b></p>
 
-                            <p class="fonte18">Para responder este instrumento você deve autoavaliar em que medida você apresenta cada uma das 19 competências seguintes, relevantes para alcançar suas metas de vendas.</p>
-                            <p class="fonte18">Procure ser o mais objetivo possível, pois as respostas serão o ponto de partida para um Programa de Desenvolvimento de suas Competências, o qual lhe permitirá alcançar suas metas de vendas com maior facilidade, e com menor tempo de dedicação (redução do ciclo de venda).</p>
+                            <p class="fonte18">Para responder este Instrumento, você deve autoavaliar em que medida você apresenta cada uma das 19 competências seguintes, relevantes para alcançar suas metas de Vendas.</p>
+                            <p class="fonte18">Procure ser o mais objetivo possível, pois as respostas serão o ponto de partida para um Programa de Desenvolvimento de suas Competências, o qual lhe permitirá alcançar suas metas de Vendas com maior facilidade, e com menor tempo de dedicação (redução do ciclo de venda).</p>
                             <p class="fonte18">Responda o Questionário de Autoavaliação das Competências de Vendas que apresentamos a seguir.</p>
                             <p class="fonte18">Antes de preencher o questionário leia atentamente as definições das 19 competências contidas nas perguntas que você responderá.</p>
 @php
 $table1 = 
 [
-"<b>1. Conhecimento do Produto.</b> Como você classificaria seu nível de conhecimento dos serviços/produtos que você vende?",
-"<b>2. Conhecimento dos Negócios do cliente.</b> Em que medida você conhece os processos de negócios, a organização, os negócios e as tendências dos negócios da empresa?",
-"<b>3. Conhecimento do Mercado.</b> Qual é seu conhecimento das necessidades e exigências do mercado em que você atua?",
-"<b>4. Informações sobre a concorrência.</b> Quão informado você está sobre os produtos e serviços concorrentes?",
-"<b>5. Inteligência Emocional.</b> Em que medida você é consciente da importância do relacionamento humano para realizar uma venda?",
-"<b>6. Habilidade para ouvir.</b> Você sabe ouvir com atenção o que outra pessoa está dizendo em termos de conteúdo e emoções envolvidas?",
-"<b>7. Perguntar, Sondar, Estimular.</b> Quão hábil você é para estimular as outras pessoas a expor, com profundidade, seus pensamentos e pontos de vista?",
-"<b>8. Sensibilidade.</b> Em que grau você é sensível aos sentimentos e as necessidades de outras pessoas?",
-"<b>9. Cooperação.</b> Quão eficiente você é para estabelecer relações nas quais ambas as partes sintam liberdade para oferecer e pedir ajuda?",
-"<b>10. Persuasão.</b> Em que medida você se sente preparado para apresentar pontos de vista e argumentos de uma maneira convincente?",
-"<b>11. Capacidade de Análise.</b> Qual é seu grau de capacidade analítica para tratar das necessidades do cliente?",
-"<b>12. Desenvolvimento de Estratégia e Planejamento.</b> Qual sua capacidade para desenvolver planos e idéias interessantes para o longo prazo?",
-"<b>13. Identificação de Oportunidades.</b> Em que medida você está atento para a identificação de novas oportunidades de negócios?",
-"<b>14. Organização Pessoal e Administração do Tempo.</b> Você é pontual, bem organizado nas apresentações e está sempre disposto para solucionar os problemas do cliente?",
-"<b>15. Criatividade na Solução de Problemas.</b> Qual é sua capacidade para desenvolver propostas criativas e soluções alternativas inovadoras para os problemas e necessidades do cliente?",
-"<b>16. Persistência no Seguimento.</b> Você é persistente, segue um plano e mantém o compromisso até os resultados serem atingidos?",
-"<b>17. Serviço como Fator Diferencial.</b> Você procura desenvolver atividades que poderiam ser consideradas como um diferencial em relação à concorrência?",
-"<b>18. Ajuste das Propostas.</b> Em que medida suas propostas atendem as verdadeiras necessidades de seus clientes?",
-"<b>19. Confiança Pessoal.</b> Acredita que você transmite confiança?"
+    "1. <b>Conhecimento do Produto.</b> Como você classificaria seu nível de conhecimento dos serviços/produtos que você vende?",
+"2. <b>Conhecimento dos Negócios do cliente.</b> Em que medida você conhece os processos de negócios, a organização, os negócios e as tendências dos negócios da empresa?",
+"3. <b>Conhecimento do Mercado.</b> Qual é o seu conhecimento das necessidades e exigências do Mercado em que você atua?",
+"4. <b>Informações sobre a concorrência.</b> Quão informado você está sobre os produtos e serviços concorrentes?",
+"5. <b>Inteligência Emocional.</b> Em que medida você é consciente da importância do relacionamento humano para realizar uma venda?",
+"6. <b>Habilidade para ouvir.</b> Você sabe ouvir com atenção o que outra pessoa está dizendo em termos de conteúdo e emoções envolvidas?",
+"7. <b>Perguntar, Sondar, Estimular.</b> O quanto hábil você é para estimular as outras pessoas a expor, com profundidade, seus pensamentos e pontos de vista?",
+"8. <b>Sensibilidade.</b> Em que grau você é sensível aos sentimentos e as necessidades de outras pessoas?",
+"9. <b>Cooperação.</b> O quanto eficiente você é para estabelecer relações nas quais ambas as partes sintam liberdade para oferecer e pedir ajuda?",
+"10. <b>Persuasão.</b> Em que medida você se sente preparado para apresentar pontos de vista e argumentos de uma maneira convincente?",
+"11. <b>Capacidade de Análise.</b> Qual é seu grau de capacidade analítica para tratar das necessidades do cliente?",
+"12. <b>Desenvolvimento de Estratégia e Planejamento.</b> Qual sua capacidade para desenvolver planos e ideias interessantes para o longo prazo?",
+"13. <b>Identificação de Oportunidades.</b> Em que medida você está atento para a identificação de novas oportunidades de negócios?",
+"14. <b>Organização Pessoal e Administração do Tempo.</b> Você é pontual, bem organizado nas apresentações e está sempre disposto para solucionar os problemas do cliente?",
+"15. <b>Criatividade na Solução de Problemas.</b> Qual é sua capacidade para desenvolver propostas criativas e soluções alternativas inovadoras para os problemas e necessidades do cliente?",
+"16. <b>Persistência no Seguimento.</b> Você é persistente, segue um plano e mantém o compromisso até os resultados serem atingidos?",
+"17. <b>Serviço como Fator Diferencial.</b> Você procura desenvolver atividades que poderiam ser consideradas como um diferencial em relação à concorrência?",
+"18. <b>Ajuste das Propostas.</b> Em que medida suas propostas atendem às verdadeiras necessidades de seus clientes?",
+"19. <b>Confiança Pessoal.</b> Você acredita que transmite confiança?"
 ];
 @endphp
 <!-- <table border=1 cellpadding="5" cellspacing="0" >
@@ -93,13 +93,13 @@ $table1 =
                             <p class="fonte18" style="margin-bottom: 0%;">1 – Mínimo grau da escala. Sua resposta neste ponto significa que esta competência está ausente em seu perfil profissional.</p>
                             <p class="fonte18" style="margin-bottom: 0%;">2 – Grau de domínio desta competência é mínimo.</p>
                             <p class="fonte18" style="margin-bottom: 0%;">3 – Muito pouco. Minha situação nesta competência é muito precária.</p>
-                            <p class="fonte18" style="margin-bottom: 0%;">4 – Pouco : É um ponto intermediário entre o “muito pouco” e o termo médio no domínio desta competência</p>
+                            <p class="fonte18" style="margin-bottom: 0%;">4 – Pouco. É um ponto intermediário entre o “muito pouco” e o termo médio no domínio desta competência.</p>
                             <p class="fonte18" style="margin-bottom: 0%;">5 – Tenho um termo médio de domínio da competência entre os extremos da escala.</p>
-                            <p class="fonte18" style="margin-bottom: 0%;">6 – Estou ligeiramente acima do termo médio da escala</p>
-                            <p class="fonte18" style="margin-bottom: 0%;">7 – Estou ganhando confiança nesta competência. Tenho certo grau de conforto</p>
+                            <p class="fonte18" style="margin-bottom: 0%;">6 – Estou ligeiramente acima do termo médio da escala.</p>
+                            <p class="fonte18" style="margin-bottom: 0%;">7 – Estou ganhando confiança nesta competência. Tenho certo grau de conforto.</p>
                             <p class="fonte18" style="margin-bottom: 0%;">8 – Apresento um bom grau de domínio nesta competência, me sinto confortável.</p>
                             <p class="fonte18" style="margin-bottom: 0%;">9 – Me sinto bastante seguro nesta competência.</p>
-                            <p class="fonte18" style="margin-bottom: 0%;">10 – Excelente. Apresento um alto nível de domínio desta competência. </p>
+                            <p class="fonte18" style="margin-bottom: 0%;">10 – Excelente. Apresento um alto nível de domínio desta competência.</p>
                             
                             <br>
                             <h3>Questionário</h3>
@@ -169,7 +169,8 @@ $table1 =
                                                         'Persistência no Seguimento',
                                                         'Serviço como Fator Diferencial',
                                                         'Ajuste das Propostas',
-                                                        'Confiança Pessoal']
+                                                        'Confiança Pessoal'
+                                                    ]
                                         @endphp
                                         @for($i=1;$i<=19;$i++)
                                         <!-- QUESTÃO 1 -->
