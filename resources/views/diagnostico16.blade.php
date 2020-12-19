5bd0f8e42cc3e31477db6ab6e5d5a84c8e6c8c62
Gustavo Silva Santana <gustavo_ssantana@hotmail.com>
2020-12-18 21:06:43 -0300

    Revisão texto diagnostico 16

--- resources/views/diagnostico16.blade.php
+++ resources/views/diagnostico16.blade.php
@@ -95,19 +95,19 @@
                     <div class="col-12">
                     <h4><center>Diagnóstico e Recomendações</center></h4>
                     <br>
-                    <p class="fonte18">Os obstáculos ou Forças Restritivas devem ser gerenciados para que o Campo de Forças formado pelas Forças Propulsoras e as Forças Restritivas, tenha maior intensidade a favor das Forças Propulsoras, como mostra a figura seguinte</p>
+                    <p class="fonte18">Os obstáculos ou Forças Restritivas devem ser gerenciados, para que o Campo de Forças formado pelas Forças Propulsoras e as Forças Restritivas tenham maior intensidade a favor das Forças Propulsoras, como mostra a figura seguinte:</p>
 
                     <center><img src="../posicionamento/public/img/instrumento16/diag_16_1.jpg" style="width: 60%;"></center><br>
 
-<p class="fonte18">Assim funciona o Campo de Forças.</p>
-<p class="fonte18">Esta é a Base Conceitual deste Instrumento. Você pode correlacionar este instrumento com o anterior das Forças Propulsoras.</p>
-<p class="fonte18">Implementando as recomendações deste instrumento e do instrumento anterior, você poderá gerenciar o <b>Campo de Forças das Vendas.</b></p>
+                    <p class="fonte18">Assim funciona o Campo de Forças.</p>
+                    <p class="fonte18">Esta é a Base Conceitual deste Instrumento. Você pode correlacionar este Instrumento com o anterior, das Forças Propulsoras.</p>
+                    <p class="fonte18">Implementando as Recomendações deste Instrumento e do Instrumento anterior, você poderá gerenciar o <b>Campo de Forças das Vendas.</b></p>
 
                     <br>
                     <p class="fonte18"><h4><center>DIAGNÓSTICO</center></h4></p>
 
-                    <p class="fonte18">No questionário de respostas deste instrumento você listou os obstáculos que aparecem com maior freqüência na etapa de NEGOCIAÇÃO das Vendas Consultivas.</p>
-                    <p class="fonte18">As Forças Restritivas (obstáculos) mais freqüentes que você listou foram classificadas e colocadas na Matriz de Forças Restritivas (R’s), gerando o seguinte resultado:</p>
+                    <p class="fonte18">No questionário de respostas deste Instrumento, você listou os obstáculos que aparecem com maior frequência, na etapa de NEGOCIAÇÃO das Vendas Consultivas.</p>
+                    <p class="fonte18">As Forças Restritivas (obstáculos) mais frequentes que você listou, foram classificadas e colocadas na Matriz de Forças Restritivas (R’s), gerando o seguinte resultado:</p>
 
                     @php
                         $forcas = [ 
@@ -153,43 +153,43 @@
 
                     <br>
 
-                    <p class="fonte18">A seguir são apresentadas novamente as definições das 14 Forças Restritivas para sua análise.</p>
+                    <p class="fonte18">A seguir, são apresentadas novamente as definições das 14 Forças Restritivas para sua análise.</p>
                     <p class="fonte18">As Forças Restritivas da Venda, também denominadas obstáculos, são todos os argumentos e atitudes do cliente que podem prejudicar nosso objetivo de vender.</p>
-                    <p class="fonte18">A seguir são identificadas e definidas as 14 Forças Restritivas da Venda</p>
+                    <p class="fonte18">A seguir, são identificadas e definidas as 14 Forças Restritivas da Venda:</p>
                     <p class="fonte18"><b>R1- Hábito</b></p>
-                    <p class="fonte18">Representa a dificuldade do cliente em aderir à nossa proposta por ela representar algo novo ou diferente do que ele conhece. Apresenta-se com máxima intensidade quando são oferecidos produtos/serviços inovadores.</p>
+                    <p class="fonte18">Representa a dificuldade do cliente em aderir à nossa proposta, por ela representar algo novo ou diferente do que ele conhece. Apresenta-se com máxima intensidade quando são oferecidos produtos/serviços inovadores.</p>
                     <p class="fonte18"><b>R2- Receio</b></p>
                     <p class="fonte18">Decorre do instinto de proteção e faz com que o cliente não aceite nossa proposta, temendo ter prejuízos.</p>
                     <p class="fonte18"><b>R3- Preço</b></p>
-                    <p class="fonte18">Tendência natural em querer pagar menos, ou obter maior prazo de pagamento do que o estabelecido.</p>
+                    <p class="fonte18">Tendência natural em querer pagar menos ou obter maior prazo de pagamento do que o estabelecido.</p>
                     <p class="fonte18"><b>R4- Reclamação</b></p>
                     <p class="fonte18">Este obstáculo se apresenta quando o cliente não está percebendo os benefícios que espera do produto ou serviço oferecido ou por qualquer outra razão que o incomode.</p>
                     <p class="fonte18"><b>R5- Detalhe</b></p>
-                    <p class="fonte18">Representa a dificuldade do cliente para compreender a nossa proposta ou parte dela. Aprofunda-se um aspecto do produto/serviço oferecido, ou das condições da proposta e perde-se a visão do conjunto. Pode traduzir insegurança e desconfiança.</p>
+                    <p class="fonte18">Representa a dificuldade do cliente para compreender a nossa proposta ou parte dela. Aprofunda-se em um aspecto do produto/serviço oferecido ou das condições da proposta e perde-se a visão do conjunto. Pode traduzir insegurança e desconfiança.</p>
                     <p class="fonte18"><b>R6- Ignorância</b></p>
-                    <p class="fonte18">Expressa falta de conhecimentos específicos em relação a nossa proposta o que impede que o cliente possa avaliá-la corretamente.</p>
+                    <p class="fonte18">Expressa falta de conhecimentos específicos em relação à nossa proposta, o que impede que o cliente possa avaliá-la corretamente.</p>
                     <p class="fonte18"><b>R7- Concorrência</b></p>
-                    <p class="fonte18">É a manifestação de desinteresse do cliente em relação a nossa proposta, em função de sua preferência por uma proposta ou produto/serviço concorrente.</p>
+                    <p class="fonte18">É a manifestação de desinteresse do cliente em relação à nossa proposta, em função de sua preferência por uma proposta ou produto/serviço concorrente.</p>
                     <p class="fonte18"><b>R8- Concorrência de Amizade</b></p>
                     <p class="fonte18">É o desinteresse do cliente na nossa proposta, em função de seu relacionamento pessoal com um vendedor concorrente.</p>
                     <p class="fonte18"><b>R9- Personalidade Difícil</b></p>
-                    <p class="fonte18">Quando o cliente manifesta agressivamente qualquer opinião contrária à nossa proposta. O cliente faz comentários que tendem a desqualificar o vendedor e o provoca consciente ou inconscientemente. Este obstáculo está vinculado a características de personalidade do cliente.</p>
+                    <p class="fonte18">Quando o cliente manifesta agressivamente qualquer opinião contrária à nossa proposta. O cliente faz comentários que tendem a desqualificar o vendedor e o provoca consciente ou inconscientemente. Este obstáculo está vinculado às características de personalidade do cliente.</p>
                     <p class="fonte18"><b>R10- Tempo</b></p>
-                    <p class="fonte18">Consiste na argumentação do cliente de que não dispõe de tempo para realizar a reunião de vendas ou de que não é o momento apropriado para comprar.</p>
+                    <p class="fonte18">Consiste na argumentação do cliente de que não dispõe de tempo para realizar a Reunião de Vendas ou de que não é o momento apropriado para comprar.</p>
                     <p class="fonte18"><b>R11- Corte do Vínculo</b></p>
-                    <p class="fonte18">É caracterizado por atitudes do cliente que mostram seu interesse por finalizar a reunião de vendas, mesmo que o vendedor não tenha concluído sua apresentação.</p>
+                    <p class="fonte18">É caracterizado por atitudes do cliente que mostram seu interesse por finalizar a Reunião de Vendas, mesmo que o vendedor não tenha concluído sua apresentação.</p>
                     <p class="fonte18"><b>R12- Decisão Dividida</b></p>
                     <p class="fonte18">Manifesta-se pelo adiamento da decisão porque o comprador afirma que não tem autoridade para decidir sozinho.</p>
                     <p class="fonte18"><b>R13- Ambiente Negativo</b></p>
-                    <p class="fonte18">Qualquer atitude do cliente ou do vendedor, que possa comprometer o campo psicológico e o diálogo de vendas.</p>
+                    <p class="fonte18">Qualquer atitude do cliente ou do vendedor, que possa comprometer o Campo Psicológico e o Diálogo de Vendas.</p>
                     <p class="fonte18"><b>R14- Atitudes Impróprias</b></p>
-                    <p class="fonte18">Refere-se a palavras ou atitudes inconvenientes do cliente que prejudicam o progresso da venda. Muitas vezes este obstáculo decorre de falhas na comunicação e a precária inteligência emocional do vendedor.</p>
+                    <p class="fonte18">Referem-se às palavras ou atitudes inconvenientes do cliente que prejudicam o Progresso da Venda. Muitas vezes, este obstáculo decorre de falhas na comunicação e a precária inteligência emocional do vendedor.</p>
                    
 
                         <br>
                         <p class="fonte18"><h4><center>RECOMENDAÇÕES</center></h4></p>
 
-                        <p class="fonte18">Abaixo se apresenta a relação das Forças Restritivas (obstáculos) em ordem decrescente de freqüência, conforme suas respostas à primeira parte deste instrumento.</p>
+                        <p class="fonte18">Abaixo, apresenta-se a relação das Forças Restritivas (obstáculos) em ordem decrescente de frequência, conforme suas respostas à primeira parte deste Instrumento.</p>
 
                         <div class="row">
                             <div class="col-12" style="">
@@ -216,14 +216,14 @@
 
                         <br>
 
-                        <p class="fonte18">A primeira recomendação é que você memorize as definições dos obstáculos, de modo que você possa identificá-los assim que apareçam, apoiado na sua memória hábito.</p>
-                        <p class="fonte18">É a mesma recomendação que fizemos com relação às Forças Propulsoras.</p>
-                        <p class="fonte18">Apenas lembrando: memória hábito é aquela memória que lhe permite fazer uma ação de maneira inconsciente.</p>
+                        <p class="fonte18">A primeira Recomendação é que você memorize as definições dos obstáculos, de modo que você possa identificá-los assim que apareçam, apoiado na sua memória-hábito.</p>
+                        <p class="fonte18">É a mesma Recomendação que fizemos com relação às Forças Propulsoras.</p>
+                        <p class="fonte18">Apenas lembrando: memória-hábito é aquela memória que lhe permite fazer uma ação de maneira inconsciente.</p>
                         <p class="fonte18">Por exemplo, não pensamos como se faz para caminhar quando caminhamos.</p>
-                        <p class="fonte18">Em termos profissionais, os bons músicos de música clássica não lêem a partitura quando interpretam uma obra. Os atores de teatro ou de telenovelas representam a obra sem ler o texto, eles podem ser espontâneos e naturais na interpretação porque o texto está decorado “no inconsciente”.</p>
+                        <p class="fonte18">Em termos profissionais, os bons músicos de música clássica não leem a partitura quando interpretam uma obra. Os atores de teatro ou de telenovelas representam a obra sem ler o texto, eles podem ser espontâneos e naturais na interpretação porque o texto está decorado “no inconsciente”.</p>
                         <p class="fonte18">Os vendedores, enquanto verdadeiros profissionais de vendas, devem fazer a mesma coisa. Não é impossível: isto deve fazer parte da Profissão de Vendedor.</p>
-                        <p class="fonte18">A segunda recomendação é encontrar as Forças Propulsoras mais adequadas para neutralizar cada obstáculo ou Força Restritiva.</p>
-                        <p class="fonte18">A seguir apresentamos uma Matriz Indicativa de Forças Propulsoras para neutralizar as Forças Restritivas.</p>
+                        <p class="fonte18">A segunda Recomendação é encontrar as Forças Propulsoras mais adequadas para neutralizar cada obstáculo ou Força Restritiva.</p>
+                        <p class="fonte18">A seguir, apresentamos uma Matriz Indicativa de Forças Propulsoras para neutralizar as Forças Restritivas.</p>
                         
                         <br>
 
@@ -271,14 +271,14 @@
 
                         <br>
 
-                        <p class="fonte18">Lembre que quem vai gerenciar o CAMPO DE FORÇAS entre as Forças Propulsoras e as Forças Restritivas é a <b>Força Propulsora 17.</b></p>
-                        <p class="fonte18"><b>A Força Propulsora 17</b> monitora em tempo real como você está conduzindo o processo de vendas.</p>
-                        <p class="fonte18"><b>A Força Propulsora 17</b> nos indica as Forças Propulsoras que devem ser usadas, tanto na etapa de <b>Exposição</b> do produto como na etapa da <b>Negociação</b>, para neutralizar os obstáculos.</p>
-                        <p class="fonte18">Especificamente fazemos referência ao Instrumento 5 – Meu Conhecimento da Linha de produtos que vendo e ao Instrumento 6 – Meu Conhecimento dos produtos concorrentes.</p>
+                        <p class="fonte18">Lembre-se que quem vai gerenciar o CAMPO DE FORÇAS entre as Forças Propulsoras e as Forças Restritivas é a <b>Força Propulsora 17.</b></p>
+                        <p class="fonte18"><b>A Força Propulsora 17</b> monitora em tempo real como você está conduzindo o Processo de Vendas.</p>
+                        <p class="fonte18"><b>A Força Propulsora 17</b> nos indica as Forças Propulsoras que devem ser usadas, tanto na etapa de <b>Exposição</b> do produto como na etapa da Negociação, para neutralizar os obstáculos.</p>
+                        <p class="fonte18">Especificamente, fazemos referência ao <b>Instrumento 5 – Meu Conhecimento da Linha de Produtos que Vendo</b> e ao <b>Instrumento 6 – Meu Conhecimento dos Produtos Concorrentes.</b></p>
                         <p class="fonte18">Procure montar sua própria estratégia de Forças Propulsoras para neutralizar os obstáculos.</p>
-                        <p class="fonte18">Treine, dedique tempo para implementar as recomendações deste instrumento!</p>
-                        <p class="fonte18">Um musico profissional de musica erudita treina 10 horas por dia, para que sua apresentação seja perfeita.</p>
-
+                        <p class="fonte18">Treine, dedique tempo para implementar as Recomendações deste Instrumento!</p>
+                        <p class="fonte18">Um músico profissional de música erudita treina 10 horas por dia, para que sua apresentação seja perfeita!</p>
+                        <p class="fonte18">Este deve ser o estado de espírito do vendedor de Vendas Consultivas!</p>
 
 <!-- RESPOSTA DE ACORDO COM REQUISIÇÃO -->
                         <!-- <p class="fonte18"><h4>Diagnóstico - $retorno['titulo']</h4></p>
